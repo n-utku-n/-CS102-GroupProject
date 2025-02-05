f03c6c73281303e839e49cb5bf99cd54fc1631e8
Sıla <silab@ug.bilkent.edu.tr>
2025-02-05 17:23:06 +0300

    max min son umarım

---
+++ ArrayOperations.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+
+public class ArrayOperations {
+    public int[] generateArray(int size) {
+      Random random = new Random();
+      int[] array = new int[size]; 
+      for (int i = 0; i < size; i++) {
+        array[i] = random.nextInt(101); 
+      } 
+      return array; 
+    }
+
+    public int[] displayArray(){
+
+    }
+
+    public int[] calculateAverageDifference(){
+
+    }
+
+    public int showAverage(){
+
+    }
+
+    public int sumOfOddIndices(){
+
+    }
+
+    public int sumOfEvenIndices(){
+
+    }
+
+    public static void findMax (int [] array) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length ; i++){
+            if (array [i] > max){
+                max = array [i];
+            }
+        }
+        System.out.println("Maximum of the array: " + max);
+    }
+    
+    public static void findMin (int [] array) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length ; i++){
+            if (array [i] < min){
+                min = array [i];
+            }
+        }
+        System.out.println("Minimum of the array: " + min);
+
+    }
+}
