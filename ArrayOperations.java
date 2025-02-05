3b9937734f2a53693393db9df26f056ca9a4fdc1
Elif Bozkurt <elfbzkrtt07@gmail.com>
2025-02-05 17:21:09 +0300

    Update ArrayOperations.java

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -1,6 +1,11 @@
 public class ArrayOperations {
-    public int[] generateRandomArray(){
-
+    public int[] generateArray(int size) {
+      Random random = new Random();
+      int[] array = new int[size]; 
+      for (int i = 0; i < size; i++) {
+        array[i] = random.nextInt(101); 
+      } 
+      return array; 
     }
 
     public int[] displayArray(){
